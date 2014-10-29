f2074853c50f333c80e38a2e2f901b4b19e748d2
Sebastian Wehrmann <sw@gocept.com>
2014-10-29 15:00:45 +0100

    Remove stale bytecode files on test collection.

--- test_plugin.py
+++ test_plugin.py
@@ -1,6 +1,26 @@
+import pytest
+
 pytest_plugins = "pytester",
 
 
 def test_version():
     import plugin
     assert plugin.__version__
+
+
+@pytest.mark.parametrize("ext", ['pyc', 'pyo'])
+def test_plugin_removes_pyc_files(testdir, ext):
+    foo = testdir.makefile('py', foo='')
+    fooco = testdir.makefile(ext, foo='')
+    bar = testdir.makefile(ext, bar='')
+
+    assert len(testdir.tmpdir.listdir()) == 3
+
+    result = testdir.runpytest("-v")
+
+    result.stdout.fnmatch_lines([
+        "Removing stale bytecode file *bar.{}".format(ext),
+    ])
+    assert not bar.exists()
+    assert fooco.exists()
+    assert foo.exists()
