31002f2db683a1eb29dd43498a817e699fbdc9e5
Sebastian Wehrmann <sebastian@wehrmann.de>
2014-10-29 15:19:29 +0100

    Ignore PYTEST pyc and pyo files.

--- plugin.py
+++ plugin.py
@@ -11,6 +11,8 @@ compiled_suffixes = '.pyc', '.pyo'
 def pytest_collect_file(path, parent):
     if path.ext not in compiled_suffixes:
         return
+    if path.fnmatch('*PYTEST.py[co]'):
+        return
     if os.path.exists(str(path)[:-1]):
         return
     print("\nRemoving stale bytecode file %s" % path)
