9df5cf62c775db1698ffcd45d2e96c8209011a91
Sebastian Wehrmann <sebastian@wehrmann.de>
2014-10-29 15:00:45 +0100

    Remove stale bytecode files on test collection.

--- plugin.py
+++ plugin.py
@@ -1,5 +1,17 @@
 import pkg_resources
+import os.path
 
 
 __version__ = pkg_resources.get_distribution(
     'pytest-remove-stale-bytecode').version
+
+compiled_suffixes = '.pyc', '.pyo'
+
+
+def pytest_collect_file(path, parent):
+    if path.ext not in compiled_suffixes:
+        return
+    if os.path.exists(str(path)[:-1]):
+        return
+    print("\nRemoving stale bytecode file %s" % path)
+    path.remove()
