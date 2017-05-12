0a1d42bccc0a6fc7b40dd0284eedafff6b7a57cb
Michael Howitz <mh@gocept.com>
2017-05-12 08:12:55 +0200

    Remove redundant classifiers.

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ Intended Audience :: Developers
 License :: OSI Approved :: MIT License
 Natural Language :: English
 Operating System :: OS Independent
-Programming Language :: Python
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
@@ -52,11 +51,8 @@ Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
-Programming Language :: Python :: Implementation
 Programming Language :: Python :: Implementation :: CPython
 Programming Language :: Python :: Implementation :: PyPy
-Topic :: Software Development
-Topic :: Software Development :: Libraries
 Topic :: Software Development :: Libraries :: Python Modules
 Topic :: Software Development :: Testing
 """[:-1].split('\n'),
