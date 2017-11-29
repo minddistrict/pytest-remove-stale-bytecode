269a6f527579644cd9a2b962f675bbaf765fdda5
Steffen Allner <sa@gocept.com>
2017-11-29 09:12:00 +0100

    Drop support for Python 3.3.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def project_path(*names):
 
 setup(
     name='pytest-remove-stale-bytecode',
-    version='3.1.dev0',
+    version='4.0.dev0',
 
     install_requires=[
         'pytest',
@@ -47,7 +47,6 @@ Operating System :: OS Independent
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
