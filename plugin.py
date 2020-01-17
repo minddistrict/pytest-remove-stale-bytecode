82a179d81fb79e19ffc39cb37676183471ec9b75
Michael Howitz <mh@gocept.com>
2020-01-17 08:39:16 +0100

    Support Python 3.8.

--- plugin.py
+++ plugin.py
@@ -7,6 +7,7 @@ import sys
 __version__ = pkg_resources.get_distribution(
     'pytest-remove-stale-bytecode').version
 python_version = '{0.major}{0.minor}'.format(sys.version_info)
+pytest_version = pkg_resources.get_distribution('pytest').version
 
 compiled_suffixes = '.pyc', '.pyo'
 
@@ -15,8 +16,10 @@ def py_path(path):
     path = str(path)[:-1]
     path = path.replace('_PYTEST.py', '.py')
     path = path.replace('-PYTEST.py', '.py')
+    path = path.replace('-pytest-{0}.py'.format(pytest_version), '.py')
     path = path.replace('{0}__pycache__{0}'.format(os.path.sep), os.path.sep)
     path = path.replace('.cpython-{}.py'.format(python_version), '.py')
+    path = path.replace('.pypy{}.py'.format(python_version), '.py')
     return path
 
 
